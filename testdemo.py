c2f8bab6447e48cfce3cf039b2b75e32cec208f8
BkSathish <68700787+BkSathish@users.noreply.github.com>
2021-04-27 22:24:59 +0530

    Update testdemo.py

--- testdemo.py
+++ testdemo.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.title("hello world")
+st.title("BRO ellaam pannite deploy um pannitte.....aana coding la edho fauld ide la run aagudhu....web app la run aaga matidhu....pasikkudhu....kalela mass ah anuchoodren")
