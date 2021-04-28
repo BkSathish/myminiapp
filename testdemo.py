6305e12e5b892e1e24bcf6ec9be4c7cb5beb1217
BkSathish <68700787+BkSathish@users.noreply.github.com>
2021-04-28 23:20:46 +0530

    Update testdemo.py

--- testdemo.py
+++ testdemo.py
@@ -2,6 +2,6 @@ import streamlit as st
 import cv2
 
 st.title("demo")
-image=cv2.imread("Resources/skimmer1a.jpg")
+image=cv2.imread("https://github.com/BkSathish/myminiapp/blob/main/Resources/skimmer1a.jpg")
 st.image(image, caption=None, width=590)
 st.warning("updated")
