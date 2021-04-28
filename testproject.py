76aa34860ba6822a166611a4f50e2cb1a7e4055c
BkSathish <68700787+BkSathish@users.noreply.github.com>
2021-04-28 22:06:02 +0530

    Update testproject.py

--- testproject.py
+++ testproject.py
@@ -4,8 +4,8 @@ import numpy as np
 import streamlit as st
 
 
-
 st.title("ATM SKIMMER ENQUIRER")
+
 
 original = cv2.imread("Resources/skimmer1a.jpg")
 
@@ -29,19 +29,21 @@ if uploaded_file is not None:
     #difference of 2 images in the same
     diff = original.copy()
     cv2.subtract(original, new, diff)
+    result= not np.any(diff)
 
     # converting the difference into grascale
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
     # increasing the size of differences so we can capture them all
+
+
     for i in range(0, 3):
         dilated = cv2.dilate(gray.copy(), None, iterations=i + 1)
 
-    # threshold the gray image to binarise it. Anything pixel that has
-    # value more than 3 we are converting to white
-    # (remember 0 is black and 255 is absolute white)
-    # the image is called binarised as any value less than 3 will be 0 and
-    # all values equal to and more than 3 will be 255
+        # threshold the gray image to binarise it. Anything pixel that has
+        # value more than 3 we are converting to white
+        # the image is called binarised as any value less than 3 will be 0 and
+        # all values equal to and more than 3 will be 255
     (T, thresh) = cv2.threshold(dilated, 3, 255, cv2.THRESH_BINARY)
 
     # now we need to find contours in the binarised image
@@ -49,18 +51,30 @@ if uploaded_file is not None:
     cnts = imutils.grab_contours(cnts)
     area=[]
     i=[]
-    for c in cnts:
-        # fit a bounding box to the contour
-        (x, y, w, h) = cv2.boundingRect(c)
+    if result is True:
+        st.warning("NO SKIMMER DETECTED")
+        st.warning("NOW YOU CAN USE YOUR CARD SAFELY")
+    else:
+        for c in cnts:
+            # fit a bounding box to the contour
+            (x, y, w, h) = cv2.boundingRect(c)
 
-        area=int(cv2.contourArea(c))
-        if area>11000 and area<15000:
-            print(area)
-            cv2.rectangle(new, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            area=int(cv2.contourArea(c))
+            if area>11000 and area<15000:
+                #
+                print(area)
+                cv2.rectangle(new, (x, y), (x + w, y + h), (255,0,0), 2)
+                cv2.putText(new,"skimmer detected",((w//2)+x-200,(h//2)+y-200),cv2.FONT_HERSHEY_SIMPLEX,1.5,(255,0,0),1 )
+                st.error("ALERT")
 
+                st.image(new, caption=None, width=590)
     # uncomment below 2 lines if you want to
     # view the image press any key to continue
     # write the identified changes to disk
     #cv2.imshow("changes", new)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
+
+
+else:
+    st.warning("please upload image")
